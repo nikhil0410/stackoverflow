24b87e53fe8fa50b1dc3a1df0c9cba0a9b139ebd
nikhil-vinove <nikhil.gupta@mail.vinove.com>
2018-11-12 14:49:25 +0530

    added spase to the setting

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'clqw=6pxa8zn=%7mk1&a6dr&f4ddr8$ak)c@yxsv^sr=ot#z-t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
