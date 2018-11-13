3903e4dbab174300faa998c77a3ee36c8975b355
nikhil-vinove <nikhil.gupta@mail.vinove.com>
2018-11-13 17:48:17 +0530

    model added for the answer

--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from questions.models import Question
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Answers(models.Model):
+	question_id 		= models.ForeignKey(Question,on_delete=None)
+	user 				= models.ForeignKey(User,on_delete=None)
+	answer_description 	= models.TextField()
+	upvote_count		= models.IntegerField()
+	downvote_count		= models.IntegerField()
+	featured        	= models.BooleanField(default=False)
+	active          	= models.BooleanField(default=True)
+	timestamp      	 = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.question_id
+
