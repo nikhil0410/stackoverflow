398239289a56a3cf3d286aefc3df609825295ac6
nikhil04 <ernikhilgupta2015@gmail.com>
2018-11-15 21:02:25 +0530

    added to admin, answer comment and question comment

--- answers/admin.py
+++ answers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Answers
+from .models import Answers,AnswersComment
 
 admin.site.register(Answers)
+admin.site.register(AnswersComment) 
 # Register your models here.
